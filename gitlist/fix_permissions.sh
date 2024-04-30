4fd7bead3a43f1d039135dc2f9d8ab9264140c3d
datagutten <datagutten@datagutten.net>
2024-04-30 21:52:49 +0200

    First version

---
+++ gitlist/fix_permissions.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+docker exec switchinfo_docker-gitlist-1 chown -R www-data /var/www/Config
---
+++ gitlist/show_log.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+docker exec -it switchinfo_docker-gitlist-1 cat var/log/prod.log

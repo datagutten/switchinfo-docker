e9d3e3987e327f3de02c144b9fd6333e06ba3cd4
datagutten <datagutten@datagutten.net>
2024-05-17 15:36:15 +0200

    Use apache instead of nginx

--- nginx/gitlist.sh
+++
@@ -1,8 +0,0 @@
-mkdir /home/app/web/gitlist
-cd /home/app/web/gitlist || exit
-
-wget "https://github.com/klaussilveira/gitlist/releases/download/2.0.0/gitlist-2.0.0.zip"
-unzip gitlist-2.0.0.zip
-# cp  ./config ./config
-git config --global --add safe.directory /home/app/web/config_backup
-
